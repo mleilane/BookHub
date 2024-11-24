9e15e9021fed2e848c4e0f5258119488e7c6bde6
mleilane <leilane94silva@yahoo.com>
2024-11-24 19:09:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Data.SqlClient;
+
+#region Teste da conexao com BD
+//class Program
+//{
+//    static void Main()
+//    {
+//        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["BookHubDBConnection"].ConnectionString;
+
+//        using (SqlConnection connection = new SqlConnection(connectionString))
+//        {
+//            try
+//            {
+//                connection.Open();
+//                Console.WriteLine("Conexão bem-sucedida!");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"Erro ao conectar: {ex.Message}");
+//            }
+//        }
+//    }
+//}
+#endregion
