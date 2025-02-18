9f753e75487b273ec54d028edb92c5b96b385458
mleilane <leilane94silva@yahoo.com>
2025-02-18 19:29:02 -0300

    feat: Implementação da LivroService e início da LivroController

---
+++ Controller/LivroController.cs
@@ -0,0 +1,83 @@
+﻿using BookHub.Models;
+using BookHub.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BookHub.Controller
+{
+    internal class LivroController
+    {
+        private LivroService _livroService;
+
+        public bool CadastrarLivro(Livro livro)
+        {
+            // Chama a service para verificar a existência do livro e tentar cadastrá-lo
+            bool resultado = _livroService.CadastrarLivro(livro);
+
+            if (resultado)
+            {
+                // Exibe uma MessageBox indicando que o livro foi cadastrado com sucesso
+                MessageBox.Show(
+                    "Livro cadastrado com sucesso!",
+                    "Cadastro Realizado",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+                return true; // Operação bem-sucedida
+            }
+            else
+            {
+                // Exibe a MessageBox caso o livro já exista no banco
+                var resultadoMensagem = MessageBox.Show(
+                    "O livro já está cadastrado. Deseja adicionar mais um na quantidade?",
+                    "Livro já cadastrado",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Question
+                );
+
+                if (resultadoMensagem == DialogResult.OK)
+                {
+                    // Se o usuário clicar em OK, tenta atualizar o livro com a nova quantidade
+                    bool atualizacao = _livroService.CadastrarLivro(livro); // Chama novamente a service para atualizar
+
+                    if (atualizacao)
+                    {
+                        // Exibe uma MessageBox indicando que a quantidade foi atualizada
+                        MessageBox.Show(
+                            "Quantidade de livros atualizada com sucesso!",
+                            "Atualização Realizada",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information
+                        );
+                        return true;
+                    }
+                    else
+                    {
+                        // Se houve um erro na atualização, exibe uma mensagem de erro
+                        MessageBox.Show(
+                            "Erro ao tentar atualizar a quantidade do livro. Tente novamente.",
+                            "Erro",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
+                        return false;
+                    }
+                }
+                else
+                {
+                    // Se o usuário clicar em Cancelar, cancela a operação
+                    return false;
+                }
+            }
+        }
+
+    }
+}
+
+
+
+
---
+++ Service/LivroService.cs
@@ -0,0 +1,67 @@
+﻿using BookHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHub.Service
+{
+
+
+    internal class LivroService
+    {
+        private LivroRepository _livroRepository;
+
+
+
+        public List<Livro> ObterTodosLivros()
+        {
+            return _livroRepository.ObterTodosLivros();
+        }
+
+        public Livro BuscarLivroPorTitulo(string titulo)
+        {
+            return _livroRepository.BuscarLivroPorTitulo(titulo);
+        }
+
+        public Livro BuscarLivroPorIsbn(string isbn)
+        {
+            return _livroRepository.BuscarLivroPorIsbn(isbn);
+        }
+
+        public Livro BuscarLivroPorAutor(string autor)
+        {
+            return _livroRepository.BuscarLivroPorAutor(autor);
+        }
+
+
+        public bool CadastrarLivro(Livro livro)
+        {
+
+            //verifica se já existe no bd
+            Livro livroExistente = _livroRepository.BuscarLivroPorTitulo(livro.Titulo);
+
+            if (livroExistente != null) //livroja existe no bd
+            {
+                //  atualiza a quantidade de livros
+                livroExistente.Quantidade += livro.Quantidade;
+                return _livroRepository.AtualizarLivro(livroExistente);
+
+            }
+
+            // Se não existir, cadastra o novo livro
+            return _livroRepository.CadastrarLivro(livro);
+        }
+
+        public bool AtualizaLivro(Livro livro)
+        {
+            return _livroRepository.AtualizarLivro(livro);
+        }
+
+        public bool ExcluirLivro(int id)
+        {
+            return _livroRepository.ExcluirLivro(id);
+        }
+    }
+}
